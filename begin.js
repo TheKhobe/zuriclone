5432099ccc23593453da4fbdef079e8f9a78dee0
Chukwudi Orakwue <84030794+TheKhobe@users.noreply.github.com>
2022-08-15 11:25:11 -1100

    Add files via upload

---
+++ begin.js
@@ -0,0 +1,16 @@
+function myFunction() {
+var first = document.getElementById("seal");
+ var dropText = document.getElementById("drop-text");
+ dropText.innerHTML = "No, the program is completely free thanks to Ingressive For Good";
+}
+function second() {
+ var well = document.getElementById("part");
+ var drop = document.getElementById("drop-texte");
+ drop.innerHTML = "You'll need a laptop, internet connection and the willingness to learn through collaboration and mentorship.";
+}
+
+function tree(){
+ var red = document.getElementById("third");
+ var dropTwo = document.getElementById("drop");
+ dropTwo.innerHTML ="Participants who join the program and stop engaging will be given the chance to catch\n up. You'll also be assigned a 'study-mate' and mentor to help you.\n However, there is the possibility of not being promoted to the next stage if you remain inactive for too long";
+}
